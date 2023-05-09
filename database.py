c0f685c8a7bf64d111d0fc10702a8c50f7485686
chaowats <76882145+chaowats@users.noreply.github.com>
2023-05-09 06:02:39 +0000

    dynamic pages for each job

--- database.py
+++ database.py
@@ -3,13 +3,10 @@ import os
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
-engine = create_engine(
-  db_connection_string, 
-  connect_args={
-    "ssl": {
-      "ssl_ca": "/etc/ssl/cert.pem"
-    }
-  })
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 
 
 def load_jobs_from_db():
@@ -20,13 +17,14 @@ def load_jobs_from_db():
       jobs.append(dict(row._mapping))
     return jobs
 
+
 def load_job_from_db(id):
   with engine.connect() as conn:
     result = conn.execute(
-      text("SELECT * FROM jobs WHERE id = :val"),
-      val=id
+      text(f"SELECT * FROM jobs WHERE id = :val"),
+      {"val": id}
     )
-    rows = result.all()
+    rows = result.mappings().all()
     if len(rows) == 0:
       return None
     else:
@@ -35,10 +33,12 @@ def load_job_from_db(id):
 
 def add_application_to_db(job_id, data):
   with engine.connect() as conn:
-    query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+    query = text(
+      "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+    )
 
-    conn.execute(query, 
-                 job_id=job_id, 
+    conn.execute(query,
+                 job_id=job_id,
                  full_name=data['full_name'],
                  email=data['email'],
                  linkedin_url=data['linkedin_url'],
