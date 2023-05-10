6867efb39b54e99b68d511251c3fb66148bff3db
chaowats <76882145+chaowats@users.noreply.github.com>
2023-05-10 04:56:30 +0000

    store application in database

--- database.py
+++ database.py
@@ -20,15 +20,13 @@ def load_jobs_from_db():
 
 def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(
-      text(f"SELECT * FROM jobs WHERE id = :val"),
-      {"val": id}
-    )
-    rows = result.mappings().all()
+    result = conn.execute(text(f"SELECT * FROM jobs WHERE id = :val"),
+                          {"val": id})
+    rows = result.all()
     if len(rows) == 0:
       return None
     else:
-      return dict(rows[0])
+      return dict(rows[0]._mapping)
 
 
 def add_application_to_db(job_id, data):
@@ -36,12 +34,13 @@ def add_application_to_db(job_id, data):
     query = text(
       "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
     )
-
-    conn.execute(query,
-                 job_id=job_id,
-                 full_name=data['full_name'],
-                 email=data['email'],
-                 linkedin_url=data['linkedin_url'],
-                 education=data['education'],
-                 work_experience=data['work_experience'],
-                 resume_url=data['resume_url'])
+    conn.execute(
+      query, {
+        'job_id': job_id,
+        'full_name': data['full_name'],
+        'email': data['email'],
+        'linkedin_url': data['linkedin_url'],
+        'education': data['education'],
+        'work_experience': data['work_experience'],
+        'resume_url': data['resume_url']
+      })
